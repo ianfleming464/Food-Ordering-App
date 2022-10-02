628cb2fe913bb3d0df62fb29cbb450d3a03c2ef1
ianfleming464 <ianflemingdeveloper@gmail.com>
2022-10-02 21:38:13 +0200

    Added a checkout form component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,6 +3,7 @@ import Modal from '../UI/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 import CartContext from '../../store/cart-context';
+import Checkout from './Checkout';
 
 const Cart = props => {
   const cartCtx = useContext(CartContext);
@@ -40,6 +41,7 @@ const Cart = props => {
         <span>Total Amount</span>
         <span>{totalAmount}</span>
       </div>
+      <Checkout />
       <div className={classes.actions}>
         <button className={classes['button--alt']} onClick={props.onClose}>
           Close
